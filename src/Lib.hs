5e94d4514fd670dbd9cdba7ca097373701585b0f
whaph <aykut.yilmaz0@hm.edu>
2018-11-05 21:30:57 +0100

    #5 :: add immutable set op for 2d lists

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,29 @@
 module Lib
-    ( someFunc
+    (setIndex2, setIndex
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+setIndex
+    :: [a] -- the list
+    -> Int -- index
+    -> a -- new value
+    -> [a] -- the new list
+setIndex xs ii v
+    | ii < 0 = error "Bad index"
+    | otherwise = _setIndex xs ii v
+    where
+        _setIndex [] _ _ = error "Bad index"
+        _setIndex (_ : xs) 0 v = v : xs
+        _setIndex (x : xs) ii v = x : (setIndex xs (ii - 1) v)
+
+setIndex2
+    :: [[a]]  -- the list
+    -> Int    -- row
+    -> Int    -- column
+    -> a      -- new value
+    -> [[a]]  -- the new list
+setIndex2 list row col value =
+    let
+        (head, (mid : tail)) = splitAt row list
+        newSubList = setIndex mid col value
+    in  head ++ (newSubList : tail)
