6051574a377921ef2606ceaa3b32e9cc42c7ab89
jupiter <jkeppeler@hm.edu>
2018-12-02 15:02:44 +0100

    #10 implemented game ending condition and reveal full board

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,49 @@
-module Main where
+module Main (main, playGame) where
 
 import Board
 import Lib
+import System.Random
 
 main :: IO ()
-main = putStrLn ""
+main = do
+    putStrLn "Please enter the width of the board"
+    width <- readLn :: IO Int
+    putStrLn "Please enter the height of the board"
+    height <- readLn :: IO Int
+    putStrLn "Please enter the amount of mines"
+    mines <- readLn :: IO Int
+    gen <- newStdGen
+    playGame (board width height mines gen)
+
+playGame :: Board -> IO ()
+playGame b = do
+    putStrLn (show b)
+    putStrLn "--------------"
+    putStrLn "Enter a command"
+    command <- getLine
+    let ws = words command
+    if (length ws /= 3)
+      then error ("expected 3 args but got " ++ show (length ws))
+    else do
+      let instruction = ws!!0
+          w = read (ws!!1) :: Int
+          h = read (ws!!2) :: Int
+      let board = execute b instruction w h
+      if instruction == "reveal" && (isGameLost board w h)
+        then do
+            let b = revealBoard board (width board * height board)
+            putStrLn (show b)
+            error "Game over, you dun goofed"
+      else if instruction == "reveal" && (isGameWon board)
+        then do
+            let b = revealBoard board (width board * height board)
+            putStrLn (show b)
+            error "You won!"
+      else do
+        playGame board
+
+execute :: Board -> String -> Int -> Int -> Board
+execute b instruction width height
+    | instruction == "reveal" = revealField b width height
+    | instruction == "flag"   = flagField b width height
+    | otherwise               = error "unknown command"
--- src/Board.hs
+++ src/Board.hs
@@ -6,7 +6,7 @@ module Board (
     NeighbourCount(..),
     board,
     revealField,
-    flagField
+    flagField, getFieldContent, isGameLost, revealBoard, isGameWon
     ) where
 
 import Lib (getRandomMinePositions, getSurroundingPositions, setIndex)
@@ -26,6 +26,11 @@ instance Show NeighbourCount where
     show Eight = "8"
 
 data FieldContent = Mine | NoMine NeighbourCount
+
+instance Eq FieldContent where
+    Mine == Mine = True
+    -- TODO
+    _ == _ = False
 
 instance Show FieldContent where
     show (Mine) = "*"
@@ -47,11 +52,12 @@ data Board = Board
     {
       width :: Int,
       height :: Int,
+      numberOfMines :: Int,
       fields :: [Field]
     }
 
 instance Show Board where
-    show (Board width _ fields) = (\row -> (foldl (\acc y -> acc ++ (show y) ++ "\n") "" row)) (splitEvery width fields)
+    show (Board width _ _ fields) = (\row -> (foldl (\acc y -> acc ++ (show y) ++ "\n") "" row)) (splitEvery width fields)
 
 splitEvery :: Int -> [a] -> [[a]]
 splitEvery _ [] = []
@@ -66,7 +72,7 @@ board width height numberOfMines randomGenerator = do
     let minesPos = getRandomMinePositions width height numberOfMines randomGenerator
     let positions = [(w, h) | w <- [0..width-1], h <- [0..height-1]]
     let boardContent = map (\pos -> getFieldContent pos minesPos width height) positions
-    Board width height (map (\content -> Field content (Hidden False)) boardContent)
+    Board width height numberOfMines (map (\content -> Field content (Hidden False)) boardContent)
 
 
 getFieldContent :: (Int, Int) -> [(Int, Int)] -> Int -> Int -> FieldContent
@@ -75,7 +81,6 @@ getFieldContent pos minesPos width height = do
     let countSurroundingMines xs ys = length [x | x <- xs, x `elem` ys]
         surroundingPos = getSurroundingPositions pos width height
         numSurroundingMines = countSurroundingMines surroundingPos minesPos
-
     if (pos `elem` minesPos)
         then Mine
     else
@@ -88,16 +93,44 @@ revealField board x y = do
     let field = (fields board)!!pos
     let revealedField = Field (content field) Revealed
     let newFields = setIndex (fields board) pos revealedField
-    Board (width board) (height board) newFields
+    Board (width board) (height board) (numberOfMines board) newFields
 
 
 flagField :: Board -> Int -> Int -> Board
 flagField board x y = do
     let pos = ((width board) * x) + y
-    let field = (fields board)!!pos
+    --let field = (fields board)!!pos
+    let field = getFieldFromBoard board x y
     if ((state field) == Revealed)
       then board
       else do
         let flaggedField = Field (content field) (Hidden True)
         let newFields = setIndex (fields board) pos flaggedField
-        Board (width board) (height board) newFields
+        Board (width board) (height board) (numberOfMines board) newFields
+
+getFieldFromBoard :: Board -> Int -> Int -> Field
+getFieldFromBoard board x y = do
+    let pos = ((width board) * x) + y
+    (fields board)!!pos
+
+isGameLost :: Board -> Int -> Int -> Bool
+isGameLost board x y = do
+    let field = getFieldFromBoard board x y
+    Mine == content field
+
+
+isGameWon :: Board -> Bool
+isGameWon board = do
+     let isFieldHidden = map (\field -> if state field == Revealed then 0 else 1) (fields board)
+         numberOfHiddenFields = foldr (+) 0 isFieldHidden
+     numberOfMines board == numberOfHiddenFields
+
+
+-- TODO laesst sich bestimmt irgendwie schoen mit foldr schreiben aber ich checks nicht lel
+revealBoard :: Board -> Int -> Board
+revealBoard board 0 = revealField board 0 0
+revealBoard board index = do
+    let x = mod index (width board)
+        y = quot index (height board)
+    revealBoard (revealField board x y) (index - 1)
+
